a00f33e4694a5ecdeafd9f4a741777c3f40b22a7
Petr Kočí <petr.koci@samizdat.cz>
2024-01-23 16:07:05 +0100

    add missing image

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,10 @@ import img2 from "./assets/img/2.png";
 import img3 from "./assets/img/3.png";
 import img4 from "./assets/img/4.png";
 import img5 from "./assets/img/5.png";
+import img6 from "./assets/img/6.png";
 
-const images = [img1, img2, img3, img4, img5]
+
+const images = [img1, img2, img3, img4, img5, img6]
 
 
 import { Button } from "@/components/ui/button"
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://data.irozhlas.cz/rozdeleni-evropou-kalkulacka/",
   plugins: [react()],
   resolve: {
     alias: {
